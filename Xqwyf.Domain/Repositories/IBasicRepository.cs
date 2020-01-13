4a5113f4f3923dca6ebae990db6b0f9b12636831
xqwyf1 <xqwyf@163.com>
2020-01-13 22:53:45 +0800

    增加仓储接口

---
+++ Xqwyf.Domain/Repositories/IBasicRepository.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using JetBrains.Annotations;
+
+using Xqwyf.Domain.Entities;
+
+namespace Xqwyf.Domain.Repositories
+{
+    /// <summary>
+    /// 仓储接口
+    /// </summary>
+    /// <typeparam name="TAggregateRoot"></typeparam>
+    public interface IBasicRepository<TAggregateRoot>: IReadOnlyRepository<TAggregateRoot> 
+        where TAggregateRoot : class, IAggregateRoot
+    {
+        /// <summary>
+        /// Inserts a new entity.
+        /// </summary>
+        /// <param name="autoSave">
+        /// Set true to automatically save changes to database.
+        /// This is useful for ORMs / database APIs those only save changes with an explicit method call, but you need to immediately save changes to the database.
+        /// </param>
+        /// <param name="cancellationToken">A <see cref="T:System.Threading.CancellationToken" /> to observe while waiting for the task to complete.</param>
+        /// <param name="entity">Inserted entity</param>
+        [NotNull]
+        Task<TAggregateRoot> InsertAsync([NotNull] TAggregateRoot entity, bool autoSave = false, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Updates an existing entity. 
+        /// </summary>
+        /// <param name="autoSave">
+        /// Set true to automatically save changes to database.
+        /// This is useful for ORMs / database APIs those only save changes with an explicit method call, but you need to immediately save changes to the database.
+        /// </param>
+        /// <param name="cancellationToken">A <see cref="T:System.Threading.CancellationToken" /> to observe while waiting for the task to complete.</param>
+        /// <param name="entity">Entity</param>
+        [NotNull]
+        Task<TAggregateRoot> UpdateAsync([NotNull] TAggregateRoot entity, bool autoSave = false, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Deletes an entity.
+        /// </summary>
+        /// <param name="entity">Entity to be deleted</param>
+        /// <param name="autoSave">
+        /// Set true to automatically save changes to database.
+        /// This is useful for ORMs / database APIs those only save changes with an explicit method call, but you need to immediately save changes to the database.
+        /// </param>
+        /// <param name="cancellationToken">A <see cref="T:System.Threading.CancellationToken" /> to observe while waiting for the task to complete.</param>
+        Task DeleteAsync([NotNull] TAggregateRoot entity, bool autoSave = false, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Deletes an entity by primary key.
+        /// </summary>
+        /// <param name="id">Primary key of the entity</param>
+        /// <param name="autoSave">
+        /// Set true to automatically save changes to database.
+        /// This is useful for ORMs / database APIs those only save changes with an explicit method call, but you need to immediately save changes to the database.
+        /// </param>
+        /// <param name="cancellationToken">A <see cref="T:System.Threading.CancellationToken" /> to observe while waiting for the task to complete.</param>
+        Task DeleteAsync(object id, bool autoSave = false, CancellationToken cancellationToken = default);  //TODO: Return true if deleted
+
+    }
+}
--- Xqwyf.Domain/Repositories/IRepository.cs
+++ Xqwyf.Domain/Repositories/IRepository.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace Xqwyf.Domain.Repositories
 {
+    /// <summary>
+    /// 仓储标记，只用于标记
+    /// </summary>
     public interface IRepository
     {
 
