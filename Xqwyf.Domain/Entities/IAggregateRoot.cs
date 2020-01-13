11f240fe6919e3ca5b880591a732f3304d5fbfdc
xqwyf1 <xqwyf@163.com>
2020-01-13 16:33:05 +0800

    增加聚合接口

---
+++ Xqwyf.Domain/Entities/IAggregateRoot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xqwyf.Domain.Entities
+{
+    /// <summary>
+    /// 聚合接口
+    /// </summary>
+    public interface IAggregateRoot : IEntity
+    {
+
+    }
+}
