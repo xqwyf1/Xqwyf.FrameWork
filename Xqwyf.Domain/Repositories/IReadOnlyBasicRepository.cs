15cc120e265bcee0033d60c49ee13cbcc7fa3196
xqwyf1 <xqwyf@163.com>
2020-01-13 17:36:33 +0800

    仓储基础类

---
+++ Xqwyf.Domain/Repositories/IReadOnlyBasicRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xqwyf.Domain.Entities;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Xqwyf.Domain.Repositories
+{
+    /// <summary>
+    /// 只读仓储基础类
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    public interface IReadOnlyBasicRepository<TAggregateRoot> : IRepository
+       where TAggregateRoot : class, IAggregateRoot
+    {
+        /// <summary>
+        /// 获取所有的聚合根列表
+        /// </summary>
+        /// <param name="includeDetails">说明是否包括子实体，默认不包括</param>
+        /// <param name="cancellationToken">说明操作是否可以取消</param>
+        /// <returns></returns>
+        Task<List<TAggregateRoot>> GetListAsync(bool includeDetails = false, CancellationToken cancellationToken = default);
+        /// <summary>
+        /// 获取聚合根个数
+        /// </summary>
+        /// <param name="cancellationToken">说明操作是否可以取消</param>
+        /// <returns></returns>
+        Task<long> GetCountAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ Xqwyf.Domain/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xqwyf.Domain.Repositories
+{
+    public interface IRepository
+    {
+
+    }
+}
