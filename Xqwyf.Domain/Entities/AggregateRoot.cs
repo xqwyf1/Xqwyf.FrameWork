312680d31941ae7b80f8963e9afd6f662acbab7a
xqwyf1 <xqwyf@163.com>
2020-01-13 16:39:20 +0800

    聚合基础类新增

---
+++ Xqwyf.Domain/Entities/AggregateRoot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xqwyf.Domain.Entities
+{
+    /// <summary>
+    /// 聚合基础类
+    /// </summary>
+    public abstract class AggregateRoot : Entity, IAggregateRoot
+    {
+    }
+}
